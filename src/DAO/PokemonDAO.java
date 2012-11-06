300c81a91af9759bc98e678b8139444f3e1e9d64
Maike <maike-ps@hotmail.com>
2012-11-05 22:23:15 -0200

    05/11 - HSQL, corrigindo

--- src/DAO/PokemonDAO.java
+++ src/DAO/PokemonDAO.java
@@ -19,6 +19,60 @@ public class PokemonDAO {
 
         MySQL banco = new MySQL();
         String sql = "select * from pokemon "
+                + "inner join elemento on pokemon.elementoPrimario = elemento.id "
+                + "where pokemon.id = " + id;
+
+        ConjuntoResultados linhas = banco.executaSelect(sql);
+
+        Pokemon p = new Pokemon();
+
+        if (linhas.next()) {
+            p.setId(linhas.getInt("pokemon.id"));
+            p.setNome(linhas.getString("pokemon.nome"));
+            p.setRaridade(linhas.getInt("pokemon.raridade"));
+            p.setLevelQueEvolui(linhas.getInt("pokemon.lvlQueEvolui"));
+            p.setAtkBase(linhas.getInt("pokemon.atkBase"));
+            p.setDefBase(linhas.getInt("pokemon.defBase"));
+            p.setSpdBase(linhas.getInt("pokemon.spdBase"));
+            p.setHpBase(linhas.getInt("pokemon.hpBase"));
+            p.setElementoPrimario(linhas.getInt("pokemon.elementoPrimario"));
+            p.setElementoSecundario(linhas.getInt("pokemon.elementoSecundario"));
+            p.setElementoPrimarioString(linhas.getString("elemento.elemento"));
+            p.setBaseExp(linhas.getInt("pokemon.baseExp"));
+        }
+
+        sql = "select * from pokemon "
+                + "inner join elemento on pokemon.elementoSecundario = elemento.id "
+                + "where pokemon.id = " + id;
+
+        linhas = banco.executaSelect(sql);
+
+        if (linhas.next()) {
+            p.setElementoSecundarioString(linhas.getString("elemento.elemento"));
+        }
+
+        sql = "select * from pokemon "
+                + "inner join ataque on pokemon.idAtaque = ataque.id "
+                + "inner join elemento on ataque.elemento = elemento.id "
+                + "where pokemon.id = " + id;
+
+        linhas = banco.executaSelect(sql);
+
+        if (linhas.next()) {
+            p.setIdAtaque(linhas.getInt("ataque.id"));
+            p.setNomeAtaque(linhas.getString("ataque.nome"));
+            p.setForcaAtaque(linhas.getInt("ataque.atk"));
+            p.setElementoAtaque(linhas.getString("elemento.elemento"));
+            p.setElementoAtaqueId(linhas.getInt("elemento.id"));
+        }
+
+        return p;
+    }
+
+    public static Pokemon getPokemonVelho(int id) {
+
+        MySQL banco = new MySQL();
+        String sql = "select * from pokemon "
                 + "inner join elemento e1 on pokemon.elementoPrimario = e1.id "
                 + "inner join elemento e2 on pokemon.elementoSecundario = e2.id "
                 + "inner join ataque on pokemon.idAtaque = ataque.id "
